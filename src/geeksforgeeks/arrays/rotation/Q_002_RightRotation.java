3252ccfb08129ed52598303b08aebe1302f1556d
om-udavant <omudavant@gmail.com>
2022-01-09 08:44:01 +0530

    right rotation file

--- src/geeksforgeeks/arrays/rotation/Q_002_RightRotation.java
+++ src/geeksforgeeks/arrays/rotation/Q_002_RightRotation.java
@@ -1,6 +1,23 @@
 package geeksforgeeks.arrays.rotation;
 
+import java.util.Arrays;
+
 public class Q_002_RightRotation {
-//    askhdjashdkjhasdkhaskjdhkjashd
-// skjdhfkushdfkhs
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+
+        rotate(arr, arr.length);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void rotate(int[] arr, int length) {
+
+        int temp = arr[arr.length-1];
+        for(int i = arr.length-1; i > 0 ;i--){
+            arr[i] = arr[i-1];
+        }
+        arr[0] = temp;
+    }
 }
