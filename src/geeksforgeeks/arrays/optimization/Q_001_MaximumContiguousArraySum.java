c64973f59a037ef7a7c98fa524b3013e5e7db9e1
om-udavant <omudavant@gmail.com>
2022-01-14 10:03:31 +0530

    maximum contiguous array sum

---
+++ src/geeksforgeeks/arrays/optimization/Q_001_MaximumContiguousArraySum.java
@@ -0,0 +1,30 @@
+package geeksforgeeks.arrays.optimization;
+
+public class Q_001_MaximumContiguousArraySum {
+
+    public static void main(String[] args){
+
+        int[] arr = {-2, -3, 4, -1, -2, 1, 5, -3};
+
+        System.out.println("Maximum Contiguous Array sum is " + maxArraySum(arr));
+    }
+
+    private static int maxArraySum(int[] arr) {
+
+        int n = arr.length;
+        int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
+
+        for(int i = 0; i < n; i++){
+            max_ending_here = max_ending_here + arr[i];
+
+            if(max_ending_here > max_so_far){
+                max_so_far = max_ending_here;
+            }
+            if(max_ending_here < 0){
+                max_ending_here = 0;
+            }
+        }
+
+        return max_so_far;
+    }
+}
