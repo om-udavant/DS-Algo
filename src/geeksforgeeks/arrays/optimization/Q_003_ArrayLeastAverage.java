94efafab3aa412c6591ac3a2017ec43b1d42d33e
om-udavant <omudavant@gmail.com>
2022-01-14 16:43:46 +0530

    array least with average

---
+++ src/geeksforgeeks/arrays/optimization/Q_003_ArrayLeastAverage.java
@@ -0,0 +1,37 @@
+package geeksforgeeks.arrays.optimization;
+
+public class Q_003_ArrayLeastAverage {
+
+    public static void main(String[] args){
+        int[] arr = {3, 7, 90, 20, 10, 50, 40};
+        int n = arr.length, k = 3;
+
+        findMinAvgSubarray(arr, n, k);
+    }
+
+    private static void findMinAvgSubarray(int[] arr, int n, int k) {
+
+//        if(n < k){
+//            return;
+//        }
+
+        int res_index = 0;
+        int curr_sum = 0;
+
+        for (int i = 0; i < k; i++){
+            curr_sum += arr[i];
+        }
+
+        int min_sum = curr_sum;
+
+        for (int i = k; i < n; i++) {
+            curr_sum += arr[i] - arr[i - k];
+
+            if (curr_sum < min_sum) {
+                min_sum = curr_sum;
+                res_index = (i - k + 1);
+            }
+        }
+        System.out.println("Subarray between [" + res_index + ", " + (res_index + k - 1) + "] has minimum average");
+    }
+}
