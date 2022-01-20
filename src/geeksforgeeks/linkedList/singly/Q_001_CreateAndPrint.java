118bbfd781c21994c3ae9aae533f8da582fc0a41
om-udavant <omudavant@gmail.com>
2022-01-20 10:23:32 +0530

    create and print a linkedList

---
+++ src/geeksforgeeks/linkedList/singly/Q_001_CreateAndPrint.java
@@ -0,0 +1,38 @@
+package geeksforgeeks.linkedList.singly;
+
+public class Q_001_CreateAndPrint {
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+        Node(int d)
+        {
+            this.data = d;
+            next = null;
+        }
+    }
+
+    public void printList()
+    {
+        Node n = head;
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Q_001_CreateAndPrint llist = new Q_001_CreateAndPrint();
+
+        llist.head = new Node(1);
+        Node second = new Node(2);
+        Node third = new Node(3);
+
+        llist.head.next = second;
+        second.next = third;
+
+        llist.printList();
+    }
+}
