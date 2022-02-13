ea54ec89d398982f31270a7e0f0cf74ff93f9808
om-udavant <omudavant@gmail.com>
2022-02-13 11:45:42 +0530

    iterative postOrder using two stacks

--- src/geeksforgeeks/stack/mics/Q_001_IterativePostorderUsingTwoStacks.java
+++ src/geeksforgeeks/stack/mics/Q_001_IterativePostorderUsingTwoStacks.java
@@ -2,6 +2,8 @@ package geeksforgeeks.stack.mics;
 
 import java.util.Stack;
 
+//TODO revisit
+
 public class Q_001_IterativePostorderUsingTwoStacks {
 
     static class Node{
@@ -15,16 +17,16 @@ public class Q_001_IterativePostorderUsingTwoStacks {
 
     public static void main(String[] args){
 
-        Node head = null;
-        head = new Node(1);
-        head.left = new Node(2);
-        head.right = new Node(3);
-        head.left.left = new Node(4);
-        head.left.right = new Node(5);
-        head.right.left = new Node(6);
-        head.right.right = new Node(7);
+        Node root = null;
+        root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
 
-        postOrderItretive(head);
+        postOrderItretive(root);
     }
 
     private static void postOrderItretive(Node head) {
