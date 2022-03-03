88c1fc90eca473648e67663b838c71fcaef06a58
om-udavant <omudavant@gmail.com>
2022-03-03 19:57:06 +0530

    number line jump

---
+++ src/hackerrank/easy/Q_013_NumberLineJumps.java
@@ -0,0 +1,66 @@
+package hackerrank.easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q_013_NumberLineJumps {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the postion of first kangaroo ");
+        int x1 = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("No of steps jumped by first kangaroo");
+        int v1 = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("Enter the postion of second kangaroo ");
+        int x2 = Integer.parseInt(bufferedReader.readLine());
+
+        System.out.println("No of steps jumped by second kangaroo");
+        int v2 = Integer.parseInt(bufferedReader.readLine());
+
+        String result = kangaroo(x1, v1, x2, v2);
+        System.out.println(result);
+
+        bufferedReader.close();
+    }
+
+    private static String kangaroo(int x1, int v1, int x2, int v2) {
+
+        if(x1 < x2){
+            if(v1 <= v2){
+                return "NO";
+            }
+            else{
+                while(x1 <= x2){
+                    if(x1 == x2){
+                        return "YES";
+                    }
+
+                    x1 = x1 + v1;
+                    x2 = x2 + v2;
+                }
+
+                return "NO";
+            }
+        }
+        else{
+            if(v2 <= v1){
+                return "NO";
+            }
+            else{
+                while(x2 <= x1){
+                    if(x1 == x2){
+                        return "YES";
+                    }
+
+                    x1 = x1 + v1;
+                    x2 = x2 + v2;
+                }
+
+                return "NO";
+            }
+        }
+    }
+}
