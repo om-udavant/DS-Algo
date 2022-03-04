6108b8df9c235e63fb62b48159ed94debd35d63b
om-udavant <omudavant@gmail.com>
2022-03-04 19:24:33 +0530

    question

--- src/hackerrank/easy/Q_012_AppleAndOrange.java
+++ src/hackerrank/easy/Q_012_AppleAndOrange.java
@@ -8,6 +8,27 @@ import java.util.List;
 
 public class Q_012_AppleAndOrange {
 
+    /*
+     * Complete the 'countApplesAndOranges' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER s
+     *  2. INTEGER t
+     *  3. INTEGER a
+     *  4. INTEGER b
+     *  5. INTEGER_ARRAY apples
+     *  6. INTEGER_ARRAY oranges
+     *
+     * Where s is the start point, and t is the endpoint of the house.
+     * The apple tree is to the left of the house, and the orange tree is to its right.
+     * Assume the trees are located on a single point, where the apple tree is at point a,
+     * and the orange tree is at point b.
+     * When a fruit falls from its tree, it lands d units of distance from its tree of origin along the x-axis.
+     * A negative value of d means the fruit fell d units to the tree's left, and a positive value of d means it falls
+     * d units to the tree's right.
+     * Given the value of d for m apples and n oranges, determine how many apples and oranges will fall on house.
+     */
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
