a95ab4c20cdba96410b03f6d876796a74ff90fad
om-udavant <omudavant@gmail.com>
2022-03-04 19:29:23 +0530

    question

--- src/hackerrank/easy/Q_013_NumberLineJumps.java
+++ src/hackerrank/easy/Q_013_NumberLineJumps.java
@@ -5,6 +5,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Q_013_NumberLineJumps {
+
+    /*
+     * Complete the 'kangaroo' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER x1
+     *  2. INTEGER v1
+     *  3. INTEGER x2
+     *  4. INTEGER v2
+     *
+     * The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+     * The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+     * You have to figure out a way to get both kangaroos at the same location at the same time
+     * as part of the show. If it is possible, return YES, otherwise return NO.
+     *
+     */
+
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
