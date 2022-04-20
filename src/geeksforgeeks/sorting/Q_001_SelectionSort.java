f9124d04c9ff7ca6c27083e993742a6b22cc05e3
om-udavant <omudavant@gmail.com>
2022-04-20 23:42:23 +0530

    selection sort

--- src/geeksforgeeks/sorting/Q_001_SelectionSort.java
+++ src/geeksforgeeks/sorting/Q_001_SelectionSort.java
@@ -14,17 +14,25 @@ public class Q_001_SelectionSort {
         int n = arr.length;
 
         for (int i = 0; i < n - 1; i++) {
+            int min = i;
 
             for (int j = i + 1; j < n; j++) {
 
-                if (arr[i] > arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                if(arr[j] < arr[min]){
+                    min = j;
                 }
+
+//                if (arr[i] > arr[j]) {
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
 
             }
 
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = arr[min];
         }
         return arr;
     }
