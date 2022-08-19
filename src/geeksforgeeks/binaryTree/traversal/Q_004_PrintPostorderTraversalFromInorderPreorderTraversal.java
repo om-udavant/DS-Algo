695f1303802f2da6e5a389b5e37fd3c151273fa1
om-udavant <omudavant@gmail.com>
2022-08-19 14:41:02 +0530

    print postorder traversal from given inorder and preorder traversals

---
+++ src/geeksforgeeks/binaryTree/traversal/Q_004_PrintPostorderTraversalFromInorderPreorderTraversal.java
@@ -0,0 +1,39 @@
+package geeksforgeeks.binaryTree.traversal;
+
+public class Q_004_PrintPostorderTraversalFromInorderPreorderTraversal {
+
+    public static void main(String[] args){
+        int in[] = { 4, 2, 5, 1, 3, 6 };
+        int pre[] = { 1, 2, 4, 5, 3, 6 };
+        int len = in.length;
+
+        printPost(in, pre, 0, len - 1);
+    }
+
+    static int preIndex = 0;
+
+    private static void printPost(int[] in, int[] pre, int inStrt, int inEnd) {
+
+        if(inStrt > inEnd){
+            return;
+        }
+
+        int inIndex = search(in, inStrt, inEnd, pre[preIndex++]);
+
+        printPost(in, pre, inStrt, inIndex - 1);
+
+        printPost(in, pre, inIndex + 1, inEnd);
+
+        System.out.print(in[inIndex] + " ");
+    }
+
+    private static int search(int[] in, int startIn, int endIn, int data){
+        int i = 0;
+        for(i = startIn; i < endIn; i++){
+            if(in[i] == data){
+                return i;
+            }
+        }
+        return i;
+    }
+}
