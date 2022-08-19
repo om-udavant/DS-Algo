675d6921d6e1c09967e364be7227d8aef3c13e07
om-udavant <omudavant@gmail.com>
2022-08-19 14:41:41 +0530

    print postorder traversal from given inorder and preorder traversals

--- src/geeksforgeeks/binaryTree/traversal/Q_004_PrintPostorderTraversalFromInorderPreorderTraversal.java
+++ src/geeksforgeeks/binaryTree/traversal/Q_004_PrintPostorderTraversalFromInorderPreorderTraversal.java
@@ -2,9 +2,9 @@ package geeksforgeeks.binaryTree.traversal;
 
 public class Q_004_PrintPostorderTraversalFromInorderPreorderTraversal {
 
-    public static void main(String[] args){
-        int in[] = { 4, 2, 5, 1, 3, 6 };
-        int pre[] = { 1, 2, 4, 5, 3, 6 };
+    public static void main(String[] args) {
+        int in[] = {4, 2, 5, 1, 3, 6};
+        int pre[] = {1, 2, 4, 5, 3, 6};
         int len = in.length;
 
         printPost(in, pre, 0, len - 1);
@@ -14,7 +14,7 @@ public class Q_004_PrintPostorderTraversalFromInorderPreorderTraversal {
 
     private static void printPost(int[] in, int[] pre, int inStrt, int inEnd) {
 
-        if(inStrt > inEnd){
+        if (inStrt > inEnd) {
             return;
         }
 
@@ -27,10 +27,10 @@ public class Q_004_PrintPostorderTraversalFromInorderPreorderTraversal {
         System.out.print(in[inIndex] + " ");
     }
 
-    private static int search(int[] in, int startIn, int endIn, int data){
+    private static int search(int[] in, int startIn, int endIn, int data) {
         int i = 0;
-        for(i = startIn; i < endIn; i++){
-            if(in[i] == data){
+        for (i = startIn; i < endIn; i++) {
+            if (in[i] == data) {
                 return i;
             }
         }
