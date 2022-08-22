f2d8d843f2d47a5282379a2263b00b489f75fbc1
om-udavant <omudavant@gmail.com>
2022-08-22 21:03:54 +0530

    find digits

--- src/hackerrank/easy/Q_037_FindDigits.java
+++ src/hackerrank/easy/Q_037_FindDigits.java
@@ -6,14 +6,14 @@ public class Q_037_FindDigits {
      * Complete the 'findDigits' function below.
      *
      * An integer d is a divisor of an integer n if the remainder of n/d = 0.
-     * Given an integer, for each digit that makes up the integer determine
-     * whether it is a divisor. Count the number of divisors occurring within the integer.
+     * Given an integer, for each digit that makes up the integer determine whether it is a divisor.
+     * Count the number of divisors occurring within the integer.
      *
      * The function is expected to return an INTEGER.
      * The function accepts INTEGER n as parameter.
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         int n = 124;
 
@@ -27,9 +27,9 @@ public class Q_037_FindDigits {
         int temp = n;
         int count = 0;
 
-        while (temp != 0) {
+        while(temp != 0){
             int rem = temp % 10;
-            if (rem != 0 && n % rem == 0) {
+            if(rem != 0 && n % rem == 0){
                 count++;
             }
             temp = temp / 10;
