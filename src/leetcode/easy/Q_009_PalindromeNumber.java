90e168b3821fff4d8ca2fe0b1a64c900c939471d
om-udavant <omudavant@gmail.com>
2022-09-05 11:41:01 +0530

    palindrone number

--- src/leetcode/easy/Q_009_PalindromeNumber.java
+++ src/leetcode/easy/Q_009_PalindromeNumber.java
@@ -3,12 +3,12 @@ package leetcode.easy;
 public class Q_009_PalindromeNumber {
 
     /*
-    * Given an integer x, return true if x is palindrome integer.
-    * An integer is a palindrome when it reads the same backward as forward.
-    * For example, 121 is a palindrome while 123 is not.
-    * */
+     * Given an integer x, return true if x is palindrome integer.
+     * An integer is a palindrome when it reads the same backward as forward.
+     * For example, 121 is a palindrome while 123 is not.
+     * */
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 
@@ -17,7 +17,7 @@ public class Q_009_PalindromeNumber {
         int sum = 0;
         int y = x;
 
-        while(y > 0){
+        while (y > 0) {
             int rem = y % 10;
             sum = (sum * 10) + rem;
             y = y / 10;
