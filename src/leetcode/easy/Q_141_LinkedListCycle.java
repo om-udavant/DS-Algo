fb31605fbb196f96869a9d09e44c281697dd1dd1
om-udavant <omudavant@gmail.com>
2022-10-12 22:54:51 +0530

    linked list cycle

---
+++ src/leetcode/easy/Q_141_LinkedListCycle.java
@@ -0,0 +1,45 @@
+package leetcode.easy;
+
+public class Q_141_LinkedListCycle {
+
+    /*
+     *
+     * Given head, the head of a linked list, determine if the linked list has a cycle in it.
+     * There is a cycle in a linked list if there is some node in the list that can be reached again
+     * by continuously following the next pointer. Internally, pos is used to denote the index of the
+     * node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+     * Return true if there is a cycle in the linked list. Otherwise, return false.
+     *
+     * */
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null) {
+            return false;
+        }
+
+        ListNode one = head;
+        ListNode two = head.next;
+
+        while (one != two) {
+
+            if (two == null || two.next == null) {
+                return false;
+            }
+            one = one.next;
+            two = two.next.next;
+        }
+
+        return true;
+    }
+}
