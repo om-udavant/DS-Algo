0d9c7534f2a3f422a6e68c0f17fd0b02eb79eff8
om-udavant <omudavant@gmail.com>
2022-10-19 19:38:15 +0530

    insert binary tree

---
+++ src/leetcode/easy/Q_226_InvertBinaryTree.java
@@ -0,0 +1,49 @@
+package leetcode.easy;
+
+public record Q_226_InvertBinaryTree() {
+
+    /*
+     * Given the root of a binary tree, invert the tree, and return its root.
+     * */
+
+    public static void main(String[] args) {
+
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode invertTree(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+
+    }
+
+}
