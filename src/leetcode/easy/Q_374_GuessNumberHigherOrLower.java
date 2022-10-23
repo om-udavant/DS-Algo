f1029c5fcd0581de40492663acb2e8c5754acb04
om-udavant <omudavant@gmail.com>
2022-10-23 08:53:57 +0530

    guess number higher or lower

---
+++ src/leetcode/easy/Q_374_GuessNumberHigherOrLower.java
@@ -0,0 +1,47 @@
+package leetcode.easy;
+
+public class Q_374_GuessNumberHigherOrLower {
+
+    /*
+     *
+     * We are playing the Guess Game. The game is as follows:
+     * I pick a number from 1 to n. You have to guess which number I picked.
+     * Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+     * You call a pre-defined API int guess(int num), which returns three possible results:
+     *   -1: Your guess is higher than the number I picked (i.e. num > pick).
+     *    1: Your guess is lower than the number I picked (i.e. num < pick).
+     *    0: your guess is equal to the number I picked (i.e. num == pick).
+     * Return the number that I picked.
+     * */
+
+    public static void main(String[] args) {
+
+    }
+
+    public int guessNumber(int n) {
+
+        int low = 1;
+        int high = n;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (guess(mid) == 0) {
+                return mid;
+            } else if (guess(mid) == -1) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+
+        }
+
+        return high;
+
+    }
+
+    private int guess(int mid) {
+        return 0;
+    }
+}
