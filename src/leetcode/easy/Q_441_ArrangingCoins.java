6e0f21a8975a6e3a7345102a152f9502d2ff51ca
om-udavant <omudavant@gmail.com>
2022-10-26 11:06:52 +0530

    arranging coins

---
+++ src/leetcode/easy/Q_441_ArrangingCoins.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+public class Q_441_ArrangingCoins {
+
+    /*
+     *
+     *You have n coins and you want to build a staircase with these coins.
+     * The staircase consists of k rows where the ith row has exactly i coins.
+     * The last row of the staircase may be incomplete.
+     * Given the integer n, return the number of complete rows of the staircase you will build.
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public int arrangeCoins(int n) {
+
+       int[] arr = {4,3,1};
+
+       return -arr[0];
+
+    }
+}
