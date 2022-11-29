f22320f4280f03d382257ceb9640942aa23ca306
om-udavant <omudavant@gmail.com>
2022-11-29 23:32:04 +0530

    postorder traversal

---
+++ src/leetcode/easy/Q_590_NAryTreePostorderTraversal.java
@@ -0,0 +1,56 @@
+package leetcode.easy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Q_590_NAryTreePostorderTraversal {
+
+    /*
+     * Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
+     * Nary-Tree input serialization is represented in their level order traversal.
+     * Each group of children is separated by the null value
+     */
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> postorder(Node root) {
+
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pollLast();
+            output.addFirst(node.val);
+            for (Node child : node.children) {
+                stack.add(child);
+            }
+        }
+
+        return output;
+
+    }
+}
