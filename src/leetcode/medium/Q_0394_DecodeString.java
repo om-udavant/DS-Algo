917915bb7bfe38317bba2dc166800cf3b8d46a50
om-udavant <omudavant@gmail.com>
2022-12-27 06:17:12 +0530

    decode string

---
+++ src/leetcode/medium/Q_0394_DecodeString.java
@@ -0,0 +1,58 @@
+package leetcode.medium;
+
+import java.util.Stack;
+
+public class Q_0394_DecodeString {
+
+    /*
+     * Given an encoded string, return its decoded string.
+     * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times.
+     * Note that k is guaranteed to be a positive integer.
+     * You may assume that the input string is always valid; there are no extra white spaces, square brackets are well-formed, etc.
+     * Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers,
+     * k. For example, there will not be input like 3a or 2[4].
+     * The test cases are generated so that the length of the output will never exceed 105.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public String decodeString(String s) {
+
+        Stack<Integer> counts = new Stack();
+        Stack<String> result = new Stack();
+        String res = "";
+        int index = 0;
+
+        while (index < s.length()) {
+            if (Character.isDigit(s.charAt(index))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(index))) {
+                    count = 10 * count + (s.charAt(index) - '0');
+                    index += 1;
+                }
+                counts.push(count);
+            } else if (s.charAt(index) == '[') {
+                result.push(res);
+                res = "";
+                index += 1;
+            } else if (s.charAt(index) == ']') {
+                StringBuilder temp = new StringBuilder(result.pop());
+                int count = counts.pop();
+                for (int i = 0; i < count; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                index += 1;
+            } else {
+                res += s.charAt(index);
+                index += 1;
+            }
+        }
+
+        return res;
+
+    }
+}
