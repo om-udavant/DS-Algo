1a4465f15634a0b673514878f146c0960980a4fe
om-udavant <omudavant@gmail.com>
2022-12-30 09:21:50 +0530

    two sum II input array is sorted

---
+++ src/leetcode/medium/Q_0167_TwoSumIIInputArrayIsSorted.java
@@ -0,0 +1,45 @@
+package leetcode.medium;
+
+public class Q_0167_TwoSumIIInputArrayIsSorted {
+
+    /*
+     * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order,
+     * find two numbers such that they add up to a specific target number.
+     * Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+     * Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+     * The tests are generated such that there is exactly one solution. You may not use the same element twice.
+     * Your solution must use only constant extra space.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int[] ans = new int[2];
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left < right) {
+
+            int sum = numbers[left] + numbers[right];
+
+            if (sum == target) {
+                ans[0] = left + 1;
+                ans[1] = right + 1;
+                return ans;
+            } else if (sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+
+        }
+
+        return ans;
+
+    }
+}
