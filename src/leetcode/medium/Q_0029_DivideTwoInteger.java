3f42f963d11c50630dc2d0ccff97380ada1d1238
om-udavant <omudavant@gmail.com>
2023-03-12 16:50:15 +0530

    divide two integer

---
+++ src/leetcode/medium/Q_0029_DivideTwoInteger.java
@@ -0,0 +1,28 @@
+package leetcode.medium;
+
+public class Q_0029_DivideTwoInteger {
+
+    /*
+     * Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+     * The integer division should truncate toward zero, which means losing its fractional part. For example,
+     * 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+     * Return the quotient after dividing dividend by divisor.
+     * Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer
+     * range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1,
+     * then return 231 - 1, and if the quotient is strictly less than -231, then return -231.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public int divide(int dividend, int divisor) {
+
+        if (dividend == -2147483648 && divisor == -1) {
+            return (2147483647);
+        }
+
+        return dividend / divisor;
+    }
+}
