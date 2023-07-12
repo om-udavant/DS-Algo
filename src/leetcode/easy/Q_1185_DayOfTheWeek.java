d0e4e155fc40fb0d4010d9c51d7b0b623d39b045
om-udavant <omudavant@gmail.com>
2023-07-13 01:04:44 +0530

    day of the week

---
+++ src/leetcode/easy/Q_1185_DayOfTheWeek.java
@@ -0,0 +1,75 @@
+package leetcode.easy;
+
+public class Q_1185_DayOfTheWeek {
+
+    /*
+     * Given a date, return the corresponding day of the week for that date.
+     * The input is given as three integers representing the day, month and year respectively.
+     * Return the answer as one of the following values {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public String dayOfTheWeek(int day, int month, int year) {
+        String[] weekDays = {"Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday"};
+        int x = (dayToYear(year) + dayOfYear(day, month, year)) % 7;
+
+        return weekDays[x];
+    }
+
+    public int dayToYear(int year) {
+        int result = year - 1971;
+        result += (year - 1968 - 1) / 4;
+        return result;
+    }
+
+    public int dayOfYear(int day, int month, int year) {
+        int result = 0;
+        int y = 0;
+        if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) {
+            y = 1;
+        }
+        switch (month) {
+            case 1:
+                result = day;
+                break;
+            case 2:
+                result = 31 + day;
+                break;
+            case 3:
+                result = 59 + day + y;
+                break;
+            case 4:
+                result = 90 + day + y;
+                break;
+            case 5:
+                result = 120 + day + y;
+                break;
+            case 6:
+                result = 151 + day + y;
+                break;
+            case 7:
+                result = 181 + day + y;
+                break;
+            case 8:
+                result = 212 + day + y;
+                break;
+            case 9:
+                result = 243 + day + y;
+                break;
+            case 10:
+                result = 273 + day + y;
+                break;
+            case 11:
+                result = 304 + day + y;
+                break;
+            case 12:
+                result = 334 + day + y;
+                break;
+        }
+        return result;
+    }
+}
