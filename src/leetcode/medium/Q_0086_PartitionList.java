e7839271e68536b3d1e58bf718ff34e2dd7cfb45
om-udavant <omudavant@gmail.com>
2023-08-17 09:12:49 +0530

    partition list

---
+++ src/leetcode/medium/Q_0086_PartitionList.java
@@ -0,0 +1,57 @@
+package leetcode.medium;
+
+public class Q_0086_PartitionList {
+
+    /*
+     * Given the head of a linked list and a value x, partition it such that all nodes less than x come before
+     * nodes greater than or equal to x.
+     * You should preserve the original relative order of the nodes in each of the two partitions.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode beforeHead = new ListNode(0);
+        ListNode before = beforeHead;
+        ListNode afterHead = new ListNode(0);
+        ListNode after = afterHead;
+
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+
+        after.next = null;
+        before.next = afterHead.next;
+
+        return beforeHead.next;
+
+    }
+}
