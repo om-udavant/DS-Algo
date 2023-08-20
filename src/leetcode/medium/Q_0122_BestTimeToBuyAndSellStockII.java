0fe5e3ca5e767825d24ad5c30c853d88cd283d80
om-udavant <omudavant@gmail.com>
2023-08-20 10:55:16 +0530

    best time to buy and sell stock II

---
+++ src/leetcode/medium/Q_0122_BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,30 @@
+package leetcode.medium;
+
+public class Q_0122_BestTimeToBuyAndSellStockII {
+
+    /*
+     * You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+     * On each day, you may decide to buy and/or sell the stock.
+     * You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+     * Find and return the maximum profit you can achieve.
+     *
+     */
+
+    public static void main(String[] args) {
+
+    }
+
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return profit;
+
+    }
+}
